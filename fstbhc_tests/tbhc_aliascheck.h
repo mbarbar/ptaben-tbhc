f245c2732c05e4af76bcf7c7f68f9daee3c8968c
Mohamad Barbar <mbarbar@runbox.com>
2020-03-01 22:30:12 +1100

    Make TBHC macros use stores instead of loads.

--- fstbhc_tests/tbhc_aliascheck.h
+++ fstbhc_tests/tbhc_aliascheck.h
@@ -17,49 +17,49 @@ void deref(void) { }
 
 #define TBHC_MUSTALIAS(p, q) {\
     MUSTALIAS(p, q);\
-    *p;\
+    *p = *p;\
     deref();\
-    *q;\
+    *q = *q;\
     deref();\
 }
 
 #define TBHC_PARTIALALIAS(p, q) {\
     PARTIALALIAS(p, q);\
-    *p;\
+    *p = *p;\
     deref();\
-    *q;\
+    *q = *q;\
     deref();\
 }
 
 #define TBHC_MAYALIAS(p, q) {\
     MAYALIAS(p, q);\
-    *p;\
+    *p = *p;\
     deref();\
-    *q;\
+    *q = *q;\
     deref();\
 }
 
 #define TBHC_NOALIAS(p, q) {\
     NOALIAS(p, q);\
-    *p;\
+    *p = *p;\
     deref();\
-    *q;\
+    *q = *q;\
     deref();\
 }
 
 #define TBHC_EXPECTEDFAIL_MAYALIAS(p, q) {\
     EXPECTEDFAIL_MAYALIAS(p, q);\
-    *p;\
+    *p = *p;\
     deref();\
-    *q;\
+    *q = *q;\
     deref();\
 }
 
 #define TBHC_EXPECTEDFAIL_NOALIAS(p, q) {\
     EXPECTEDFAIL_NOALIAS(p, q);\
-    *p;\
+    *p = *p;\
     deref();\
-    *q;\
+    *q = *q;\
     deref();\
 }
 
